dc8a4e94e869a8f0a9e516ecfe586ded69b19abc
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2023-10-18 01:00:42 -0600

    adding gulp plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require('gulp-plumber');
 
 function css(done) {
     src("src/scss/**/*.scss") //Identifica el archivo SASS dentro de la carpeta src
+        .pipe(plumber())
         .pipe(sass()) //lo compila
         .pipe(dest("build/css")); //almacenarla en el disco duro
 
