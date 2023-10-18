3c26cee29d79176d4ce4b63adba157201d99f586
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2023-10-18 00:28:00 -0600

    app lista para trabajar scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,20 @@
-// const { src, dest, watch } = require("gulp");
-// const sass = require("gulp-sass")(require("sass"));
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
-// function css(done) {
-//     src("src/scss/app.scss") //Identifica el archivo SASS dentro de la carpeta src
-//         .pipe(sass()) //lo compila
-//         .pipe(dest("build/css")) //almacenarla en el disco duro
+function css(done) {
+    src("src/scss/**/*.scss") //Identifica el archivo SASS dentro de la carpeta src
+        .pipe(sass()) //lo compila
+        .pipe(dest("build/css")); //almacenarla en el disco duro
 
-//     done(); //callback que avisa a gulp cuando llegamos al final
+    done(); //callback que avisa a gulp cuando llegamos al final
 
-// }
-// function dev(done) {
-//     watch("src/scss/app.scss", css)
+}
 
+function dev(done) {
+    watch("src/scss/**/*.scss", css)
 
-//     done();
-// }
-
-// exports.dev = dev;
-function tarea(done) {
-    console.log("MI PRIMER TAREA");
     done();
 }
-exports.tarea = tarea;
+
+exports.css = css;
+exports.dev = dev;
